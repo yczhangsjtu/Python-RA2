99e7edd1c227ad881fee5c20aa644e1fd7b5a009
Yuncong Zhang <yczhangsjtu@gmail.com>
2023-08-19 15:06:12 +0800

    Animate the sprite.

--- Refactor/sprite_layer/main.py
+++ Refactor/sprite_layer/main.py
@@ -2,25 +2,27 @@ import pygame
 from Refactor.render_layer.elements_scene import ElementsScene
 from sprite import Sprite, SpriteData
 
+FRAME_RATE = 12
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
     sprite = Sprite(SpriteData("Refactor/data/aircmd.json"))
     sprite.set_pos(0, 0)
-    scene = ElementsScene(600, 400, [
-        sprite.generate_render_element(),
-    ])
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-
+        scene = ElementsScene(600, 400, [
+            sprite.generate_render_element(),
+        ])
         scene.draw(screen)
+        sprite.next_frame()
 
         pygame.display.flip()
+        pygame.time.wait(1000//FRAME_RATE)
 
 if __name__ == "__main__":
     main()
